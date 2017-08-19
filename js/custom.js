3f8649451240529d11c9515cd6e8b32498a9340d
shinji-abe <s.a.b.e@icloud.com>
2017-08-19 21:45:33 +0900

    rebuilding site 2017年 8月19日 土曜日 21時45分33秒 JST

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+// 旧ページの救済
+
+if ( location.pathname.match(/portfolio/) && (location.pathname.match(/learn_from_daiguard/) || location.pathname.match(/2017071501/)) ) {
+  // URL変換
+  var url = location.href.replace(/portfolio/g, "entry").slice(0,-1);
+  document.write("<p>該当のコンテンツは新しいページに移動しました。：<a href=\"" + url + "\">" + url + "</a></p>");
+  // canonical設定
+  var link = document.getElementsByTagName("link")[0];
+  link.href = url;
+  // redirect
+ 　　setTimeout("redirect()", 3000);
+ 　　function redirect(){
+ 　　 　location.href = url;
+ 　　}
+}
