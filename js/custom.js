5fb69449205c35cc6cc99a2bb302b2bb0c890e02
shinji-abe <s.a.b.e@icloud.com>
2017-08-19 21:51:02 +0900

    rebuilding site 2017年 8月19日 土曜日 21時51分02秒 JST

--- js/custom.js
+++ js/custom.js
@@ -3,12 +3,12 @@
 if ( location.pathname.match(/portfolio/) && (location.pathname.match(/learn_from_daiguard/) || location.pathname.match(/2017071501/)) ) {
   // URL変換
   var url = location.href.replace(/portfolio/g, "entry").slice(0,-1);
-  document.write("<p>該当のコンテンツは新しいページに移動しました。：<a href=\"" + url + "\">" + url + "</a></p>");
+  document.write("<p>該当のコンテンツは新しいページに移動しました。５秒後に移動します。</p><p>新URL: <a href=\"" + url + "\">" + url + "</a></p>");
   // canonical設定
   var link = document.getElementsByTagName("link")[0];
   link.href = url;
   // redirect
- 　　setTimeout("redirect()", 3000);
+ 　　setTimeout("redirect()", 5000);
  　　function redirect(){
  　　 　location.href = url;
  　　}
